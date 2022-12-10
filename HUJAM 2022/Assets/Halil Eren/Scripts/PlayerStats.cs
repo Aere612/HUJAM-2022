acb1e83925a0f08cd29c7cade32b61a9d14ea502
Ali Eren Aydın <Aere612@gmail.com>
2022-12-10 19:55:14 +0300

    assault + weapons finished

--- HUJAM 2022/Assets/Halil Eren/Scripts/PlayerStats.cs
+++ HUJAM 2022/Assets/Halil Eren/Scripts/PlayerStats.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
 
-    public float playerHp = 100.0f;
+    public float playerHp , maxPlayerHP=100f;
     public float movementSpeed = 1.0f;
     public float bulletPerSecond = 1.0f;
     public float damagePerBullet = 1.0f;
     public float bulletSpeed = 1.0f;
-
+    private void Start()
+    {
+        playerHp = maxPlayerHP;
+    }
 }
