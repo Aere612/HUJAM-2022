f98ff8f49e38db2567c660f92e650d00b2ffeddc
Altay Turan <axtay434@gmail.com>
2022-12-11 01:24:14 +0300

    Update ControllerActivator.cs

--- HUJAM 2022/Assets/Scripts/ControllerActivator.cs
+++ HUJAM 2022/Assets/Scripts/ControllerActivator.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ControllerActivator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Animator animator;
+    [SerializeField] private Player playerScript;
+    [SerializeField] private GameObject pewPewGun;
+
+    private void Update()
     {
-        
+        if (animator.isActiveAndEnabled)
+        {
+            playerScript.enabled = false;
+            pewPewGun.SetActive(false);
+            Invoke("SetAnimator", 7);
+        }
+        else
+        {
+            playerScript.enabled = true;
+            pewPewGun.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetAnimator()
     {
-        
+        animator.enabled = false;
     }
 }
