85b9c726563fc5e03ede8e395cafa662072d5617
Ali Eren Aydın <Aere612@gmail.com>
2022-12-10 12:48:20 +0300

    weapons

--- HUJAM 2022/Assets/Weapons_Scripts/Bullet.cs
+++ HUJAM 2022/Assets/Weapons_Scripts/Bullet.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float damage=1f,speed=0.1f;
     void Update()
     {
-        
+        transform.Translate(new Vector2(speed,0f));
     }
 }
--- HUJAM 2022/Assets/Weapons_Scripts/PewPew.cs
+++ HUJAM 2022/Assets/Weapons_Scripts/PewPew.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class PewPew : MonoBehaviour
 {
-    public GameObject bullet;
-    private void OnMouseDown()
+    public GameObject bullet,currentBullet;
+    public bool allowed = true;
+    private void Update()
     {
-        Instantiate(bullet);
-
+        if (Input.GetKey(KeyCode.Mouse1) && allowed)
+        {
+            allowed = false;
+            StartCoroutine(BulletSpawner());
+        }
+        
     }    
+    IEnumerator BulletSpawner()
+    {
+        currentBullet = Instantiate(bullet);
+        yield return new WaitForSeconds(1 / GameObject.Find("Player").GetComponent<Player>().bulletPerSecond);
+        allowed = true;
+    }
 }
