450ceb72b12ae468d9c422be4a4a273ba257b45c
Ali Eren Aydın <Aere612@gmail.com>
2022-12-11 16:12:59 +0300

    enemybullet

---
+++ HUJAM 2022/Assets/Weapons_Scripts/EnemyBullet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float damage=1f, speed=0.1f;
+    void Update()
+    {
+        transform.Translate(new Vector3(0f, speed, transform.position.z), Space.Self);
+    }
+}
