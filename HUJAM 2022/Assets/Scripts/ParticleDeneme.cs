0e18e103a0e35983b76279cee3b8b1bdd0549e04
Altay Turan <axtay434@gmail.com>
2022-12-11 05:32:23 +0300

    Evren Patlaması

--- HUJAM 2022/Assets/Scripts/ParticleDeneme.cs
+++ HUJAM 2022/Assets/Scripts/ParticleDeneme.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class ParticleDeneme : MonoBehaviour
 {
-
+    GameObject[] allObjects;
+    [SerializeField] List<GameObject> activetedObjects;
     [SerializeField] private ParticleSystem playerDeathParticle;
+
+    private PlayerStats playerStats;
     void Start()
     {
-        var Object = Instantiate(playerDeathParticle, this.transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
-        Destroy(Object,0.8f);
+        allObjects = FindObjectsOfType<GameObject>();
+        playerStats = GameObject.Find("Game Manager Object").GetComponent<PlayerStats>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if(playerStats.playerHp <= 0)
+        {
+            Explode();
+        }
     }
 
-    
+    void Explode()
+    {
+        foreach (var obj in allObjects)
+        {
+            obj.SetActive(false);
+        }
+        var explosion = Instantiate(playerDeathParticle, this.transform.position, Quaternion.identity);
+        foreach (var obj in activetedObjects)
+        {
+            obj.SetActive(true);
+        }
+        Destroy(explosion, 2f);
+    }
 }
