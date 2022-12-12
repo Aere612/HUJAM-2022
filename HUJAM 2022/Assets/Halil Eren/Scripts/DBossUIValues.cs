6189581a838051ca8b77b65e57441b94f582dbe1
Ali Eren Aydın <Aere612@gmail.com>
2022-12-12 13:43:14 +0300

    tutorial

---
+++ HUJAM 2022/Assets/Halil Eren/Scripts/DBossUIValues.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DBossUiValues : MonoBehaviour
+{
+    public PlayerStats playerStats;
+    public TextMeshProUGUI bossHpText, playerHpText;
+    public GameObject boss;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHpText.text = playerStats.playerHp.ToString();
+        bossHpText.text = boss.GetComponent<BossDefeatable>().bossHP.ToString();
+    }
+}
